96d5b3fcaf2a7ef81ad5a0dded9c6dee5439e729
msamedozmen <msamedozmen@gmail.com>
2024-04-04 12:52:00 +0300

    Google Play Store Analyse

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+import plotly.express as px
+import plotly.graph_objects as go
+
+st.set_page_config(page_title="Google Play Store App Analyse",layout="wide")
+
+
+
+#STYLE PART
+st.markdown("""<style>
+            .header {
+                visibility:hidden;
+            }
+            
+            
+            </style>
+            """)
+
+st.markdown("<h1> GOOGLE PLAY STORE APPLICATON ANALYSE</h1>",unsafe_allow_html=True)
+
